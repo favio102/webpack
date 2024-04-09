47b1124bfb01a52ecce659fd9eb296182d98fbb5
Favio V <favio102@protonmail.com>
2024-04-09 10:48:12 +0900

    break code

---
+++ src/app/component.service.js
@@ -0,0 +1,19 @@
+class ComponentService {
+  constructor() {
+    this.numberOneInput = document.getElementById("numberOne");
+    this.numberTwoInput = document.getElementById("numberTwo");
+    this.addValuesButton = document.getElementById("addValues");
+    this.result = document.getElementById("result");
+  }
+  getInputs() {
+    return [this.numberOneInput.value, this, this.numberTwoInput.value];
+  }
+
+  setResult(str) {
+    this.resultDiv.innerText = str;
+  }
+
+  onClick(cb) {
+    this.addValuesButton.addEventListener("click", cb);
+  }
+}
