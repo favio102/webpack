dafe22720fe416330eede6c97123091b10eb6cbf
Favio Valero <favio102@protonmail.com>
2024-04-09 06:50:37 -0700

    html loader, file-loader, clean-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,34 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash].[ext]",
+              outputPath: "imgs",
+              esModule: false,
+            },
+          },
+        ],
       },
     ],
   },
